714dbc29f78feb77c80535301d06459112c59e01
Tornikeramazashvili <mnateus@gmail.com>
2024-02-15 09:33:19 +0400

    Added: main accordion and its styles

--- scripts/app.js
+++ scripts/app.js
@@ -14,3 +14,27 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+// Accordion__________________________________________________________________________
+
+document.querySelectorAll(".accordion-header").forEach((button) => {
+  button.addEventListener("click", () => {
+    const accordionContent = button.nextElementSibling;
+
+    button.classList.toggle("active");
+
+    if (button.classList.contains("active")) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+    } else {
+      accordionContent.style.maxHeight = 0;
+    }
+
+    // Close other open accordion items
+    document.querySelectorAll(".accordion-header").forEach((otherButton) => {
+      if (otherButton !== button) {
+        otherButton.classList.remove("active");
+        otherButton.nextElementSibling.style.maxHeight = 0;
+      }
+    });
+  });
+});
