6a7e127856bee7ae94cec636ff5ab7bffe79e93c
Tornikeramazashvili <mnateus@gmail.com>
2024-02-15 12:05:46 +0400

    Fix: responsive hamburger menu styles on each device

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,14 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 // Event listeners
+
+//for clicking on the hamburger menu button
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
+// for clicking on navigation links
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
     hamburger.classList.remove("active");
@@ -15,12 +18,10 @@ document.querySelectorAll(".nav-link").forEach((n) =>
   })
 );
 
-// Accordion__________________________________________________________________________
-
+// Event listener for clicking on accordion headers
 document.querySelectorAll(".accordion-header").forEach((button) => {
   button.addEventListener("click", () => {
     const accordionContent = button.nextElementSibling;
-
     button.classList.toggle("active");
 
     if (button.classList.contains("active")) {
@@ -29,12 +30,26 @@ document.querySelectorAll(".accordion-header").forEach((button) => {
       accordionContent.style.maxHeight = 0;
     }
 
-    // Close other open accordion items
+    // Close other accordion items when one is opened
     document.querySelectorAll(".accordion-header").forEach((otherButton) => {
       if (otherButton !== button) {
         otherButton.classList.remove("active");
         otherButton.nextElementSibling.style.maxHeight = 0;
       }
     });
+  });
+});
+
+//for page load and scroll events
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    let header = document.getElementById("header");
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+      header.classList.add("header-transparent");
+    } else {
+      header.classList.remove("header-transparent");
+    }
   });
 });
